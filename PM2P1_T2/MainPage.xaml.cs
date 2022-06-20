df4e7c804f9088520ea78a82846bb7248ed38f92
Smolder98 <alicoescobar@gmail.com>
2022-06-19 23:25:26 -0600

    Add project files.

---
+++ PM2P1_T2/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using PM2P1_T2.Model;
+using PM2P1_T2.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM2P1_T2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Nombres.Text) ||
+                string.IsNullOrEmpty(Apellidos.Text) ||
+                string.IsNullOrEmpty(Edad.Text) ||
+                string.IsNullOrEmpty(Correo.Text))
+            {
+                mensaje("Aviso", "De de llenar todos los campos");
+                return;
+            }
+
+            if (int.Parse(Edad.Text) < 0)
+            {
+                mensaje("Aviso", "La edad debe se mayor a cero");
+                return;
+            }
+
+            if (!ValidateEmail(Correo.Text))
+            {
+                mensaje("Aviso", "Correo no valido");
+                return;
+            }
+
+            Persona persona = new Persona() {
+                nombres = Nombres.Text,
+                apellidos = Apellidos.Text,
+                edad = Edad.Text,
+                correo = Correo.Text
+            };
+
+            SecundaryPage secundary = new SecundaryPage();
+            secundary.BindingContext = persona;
+
+            await Navigation.PushAsync(secundary);
+        }
+
+        public bool ValidateEmail(string email)
+        {
+            Regex EmailRegex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            return EmailRegex.IsMatch(email);
+        }
+
+        public async void mensaje(string titulo, string cuerpo)
+        {
+            await DisplayAlert(titulo, cuerpo, "OK");
+        }
+
+    }
+}
---
+++ PM2P1_T2/Model/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM2P1_T2.Model
+{
+    public class Persona
+    {
+        public string nombres { set; get; }
+        public string apellidos { set; get; }
+        public string edad { set; get; }
+        public string correo { set; get; }
+    }
+}
